48942277b11daad1006b482c03e08c19e867fde8
thanhlucvip2 <doanthanhluc91bvh@gmail.com>
2022-01-12 22:28:15 +0700

    scroll view

--- App.tsx
+++ App.tsx
@@ -1,40 +1,13 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  TouchableNativeFeedbackBase,
-  View,
-} from 'react-native';
+import {View} from 'react-native';
+import VerticalScrollView from './components/verticalScrollView';
+
 export default class App extends React.Component {
   render(): React.ReactNode {
     return (
-      <View style={Styles.container}>
-        <TouchableHighlight
-          onPress={() => {
-            console.log('TouchableHighlight');
-          }}
-          onShowUnderlay={() => {
-            console.log('onShowUnderlay'); // gọi hàm 2 lần khi đè chuột xuống và mở chuột lên
-          }}
-          underlayColor="red" // màu khi click
-        >
-          <View style={Styles.button}>
-            <Text>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
+      <View>
+        <VerticalScrollView />
       </View>
     );
   }
 }
-
-const Styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: 'green',
-  },
-});
