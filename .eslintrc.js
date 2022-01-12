70ec43e7881f52bd65e01949b7c01a2cd3b32b3b
thanhlucvip2 <doanthanhluc91bvh@gmail.com>
2022-01-12 21:06:01 +0700

    Text input và keyboard

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,15 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+  rules: {
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: false,
+        html: true,
       },
-    },
-  ],
+    ],
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,115 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+import {StyleSheet, TextInput, View, Keyboard} from 'react-native';
+interface myProps {}
+interface myState {}
+export default class App extends React.Component<myProps, myState> {
+  render(): React.ReactNode {
+    return (
+      <View>
+        <TextInput
+          style={Styles.container}
+          placeholder="text input"
+          placeholderTextColor={'red'} // màu placeholder
+          // keyboardType="email-address" // kiểu nhập input android: [default, number-pad, decimal-pad, numeric, email-address ,phone-pad], ios : [ascii-capable numbers-and-punctuation url name-phone-pad twitter web-search]
+          onChangeText={text => {
+            console.log(text);
+          }} // hàm nhận vào text
+          // secureTextEntry={true} // bật ẩn mật khẩu khi nhâp
+          // multiline={true} // gõ được nhiều dòng như textArea
+          // editable={true} // không chỉnh sửa được input
+          // autoFocus={true} // lần đầu khi mở component tự động con trỏ chuột vào ô input có autoFocus
+          // returnKeyType="search" // kiểu phím return trên bàn phím android [done  go  next  search  send], ios [default emergency-call google join route yahoo]
+          // onSubmitEditing={Keyboard.dismiss} // tự ẩn bàn phím khi done (Keyboard.dismiss : hàm ẩn bàn phím)
+        />
+      </View>
+    );
+  }
+}
+const Styles = StyleSheet.create({
+  container: {
+    height: 40,
+    borderColor: 'green',
+    marginTop: 200,
+    marginHorizontal: 30,
+    paddingHorizontal: 10,
+    borderWidth: 2,
+    borderBottomColor: 'red',
+    borderBottomWidth: 2,
   },
 });
-
-export default App;
