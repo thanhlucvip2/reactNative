a49d5fa2f5be93f301508be80b80a1bf6ca982c5
thanhlucvip2 <doanthanhluc91bvh@gmail.com>
2022-01-12 22:48:47 +0700

    HorizontalScrollView

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {View} from 'react-native';
-import VerticalScrollView from './components/verticalScrollView';
+import HorizontalScrollView from './components/horizontalScrollView';
 
 export default class App extends React.Component {
   render(): React.ReactNode {
     return (
       <View>
-        <VerticalScrollView />
+        <HorizontalScrollView />
       </View>
     );
   }
